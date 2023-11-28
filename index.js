6ccbc91549a634523fa824937a7c885e2eb2ee2b
Apurva Patel <apurva.patel@nurocor.com>
2023-11-27 21:10:34 -0600

    Initialize nodejs project

---
+++ index.js
@@ -0,0 +1,11 @@
+const Koa = require("koa");
+const parser = require("koa-bodyparser");
+const cors = require("@koa/cors");
+const App = new Koa();
+const port = 3000;
+
+App.use(parser())
+    .use(cors())
+    .listen(port, () => {
+        console.log(`🚀 Server listening http://127.0.0.1:${port}/ 🚀`);
+    });

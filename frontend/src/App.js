f05f599272814dc33b9cdc0f85c55e7f3a0d4ade
danFeller <danielfeller2001@gmail.com>
2023-12-01 23:58:35 -0600

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Red = styled.div`
+  color: red
+`;
+
+function App() {
+  return (
+    <div className="App">
+      <Red>Hello World</Red>
+    </div>
+  );
+}
+
+export default App;

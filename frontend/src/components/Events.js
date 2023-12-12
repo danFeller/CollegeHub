7eb453d67211e45aeb961b514b70fe4b06500660
Apurva Patel <apurva.patel@nurocor.com>
2023-12-12 15:06:37 -0600

    Details Button

--- frontend/src/components/Events.js
+++ frontend/src/components/Events.js
@@ -150,7 +150,7 @@ function Events() {
                         </ModalRow>
                         {/* onClick={() => handleAttendeesList(events.id)} */}
                         <ModalRow>
-                            {events.organizer.id === userId ? events.attendees.length > 0 ? (<Button onClick={() => HandleAttendeesList(events.id)} >Attendees</Button>) : (<></>) : (<></>)}
+                           <Button onClick={() => HandleAttendeesList(events.id)} >Attendees</Button>
                             <Overlay isOpen={isOpen} onClose={toggleOverlay}>
                                 {attendees.map((a) => (
                                     <>
